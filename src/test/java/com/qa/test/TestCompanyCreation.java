e0cadc98a78c2c545d573e078151878e1eac59db
Jaya Sree <jayasreerayala@gmail.com>
2018-05-22 12:35:50 +0530

    some modications done

--- src/test/java/com/qa/test/TestCompanyCreation.java
+++ src/test/java/com/qa/test/TestCompanyCreation.java
@@ -1,5 +1,14 @@
 package com.qa.test;
 
+import org.testng.annotations.Test;
+
 public class TestCompanyCreation {
 
+	@Test
+	public void testCompanyCreation()
+	{
+		System.out.println("test company creation");
+	}
 }
+
+
