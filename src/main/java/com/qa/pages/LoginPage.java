abc483fe74c893c0057f24282bb4c6a24146c9e7
Jaya Sree <jayasreerayala@gmail.com>
2018-05-22 11:44:22 +0530

    first git commit for my selenium code

---
+++ src/main/java/com/qa/pages/LoginPage.java
@@ -0,0 +1,8 @@
+package com.qa.pages;
+
+public class LoginPage {
+public void loginPage()
+{
+	System.out.println("Login Page");
+}
+}
---
+++ src/main/java/com/qa/utilities/TestUtils.java
@@ -0,0 +1,9 @@
+package com.qa.utilities;
+
+public class TestUtils {
+	public void testUtils()
+	{
+		System.out.println("test utils");
+	}
+
+}
